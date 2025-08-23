652e9d01d5ce0a1e2a68d293233f18c3856f823e
Chris Carrington <chris@megafarad.com>
2025-08-22 22:06:23 -0400

    (fix) Attempting to fix ConsoleFlowLogger

--- src/logging/consoleFlowLogger.ts
+++ src/logging/consoleFlowLogger.ts
@@ -1,8 +1,17 @@
 import {FlowLogger, LogEntry} from './flowLogger';
 
 export class ConsoleFlowLogger implements FlowLogger {
+
+    constructor() {
+
+    }
+
     async log(entry: LogEntry): Promise<void> {
         console.log(JSON.stringify(entry, null, 2));
+    }
+
+    static create(): FlowLogger {
+        return new ConsoleFlowLogger();
     }
 
 }
